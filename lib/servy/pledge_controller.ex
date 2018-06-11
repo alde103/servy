c3a151481143fb8bc40ddd6faade6b5c8f0ef50b
alde103 <aldebaran_103@hotmail.com>
2018-06-10 22:09:21 -0500

    24-4 stateful server

--- lib/servy/pledge_controller.ex
+++ lib/servy/pledge_controller.ex
@@ -9,7 +9,7 @@ defmodule Servy.PledgeController do
 
   def index(conv) do
     # Gets the recent pledges from the cache
-    pledges = Servy.PledgeServer.recent_pledges()
+    pledges = Servy.PledgeServer.recent_pledge()
 
     %{ conv | status: 200, resp_body: (inspect pledges) }
   end
--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,11 +1,30 @@
 defmodule Servy.PledgeServer do
   @name :pledge_server
+
+  #Client
   def start do
     IO.puts("Starting the server")
     pid = spawn(__MODULE__, :listen_loop, [[]])
     Process.register(pid, @name)
     pid
   end
+
+  def create_pledge(name,amount) do
+    send(@name, {self(), :create_pledge, name, amount})
+    receive do {:response, status} -> status end
+  end
+
+  def recent_pledge() do
+   send(@name, {self(), :recent_pledges})
+   receive do {:response, pledges} -> pledges end
+  end
+
+  def total_pledged do
+    send(@name, {self(), :total_pledged})
+    receive do {:response, total} -> total end
+  end
+
+  #Server
   def listen_loop(state) do
     IO.puts("\n Waiting for a message...")
     receive do
@@ -18,42 +37,45 @@ defmodule Servy.PledgeServer do
       {sender, :recent_pledges} ->
         send(sender, {:response, state})
         listen_loop(state)
+      {sender, :total_pledged} ->
+        total = Enum.map(state, &elem(&1,1)) |> Enum.sum
+        send(sender, {:response, total})
+        listen_loop(state)
+      unexpected ->
+        IO.puts "Unexpected messaged: #{inspect unexpected}"
+        listen_loop(state)
     end
   end
 
-   def create_pledge(name,amount) do
-     send(@name, {self(), :create_pledge, name, amount})
-     receive do {:response, status} -> status end
-   end
 
-   def recent_pledge() do
-    send(@name, {self(), :recent_pledges})
-    receive do {:response, pledges} -> pledges end
-   end
-
-   def send_pledge_to_service(_name, _amount) do
-     {:ok, "pledge-#{:rand.uniform(1000)}"}
-   end
+  def send_pledge_to_service(_name, _amount) do
+    {:ok, "pledge-#{:rand.uniform(1000)}"}
+  end
 
    #devuelve los primeros 3 elementos
-   def sim_cache([h |_t], 2) do [h] end
+  def sim_cache([h |_t], 2) do [h] end
 
-   def sim_cache([], _n) do [] end
+  def sim_cache([], _n) do [] end
 
-   def sim_cache([h | t], n) when n < 2 do
-      [h] ++ sim_cache(t, n + 1)
-   end
+  def sim_cache([h | t], n) when n < 2 do
+     [h] ++ sim_cache(t, n + 1)
+  end
 
 end
 
-# alias Servy.PledgeServer
 
-# pid = PledgeServer.start()
+ alias Servy.PledgeServer
 
-# IO.inspect(PledgeServer.create_pledge("alde1", 10))
-# IO.inspect(PledgeServer.create_pledge("alde2", 10))
-# IO.inspect(PledgeServer.create_pledge("alde3", 10))
-# IO.inspect(PledgeServer.create_pledge("alde4", 10))
-# IO.inspect(PledgeServer.create_pledge("alde5", 10))
+ pid = PledgeServer.start()
+ send(pid, {:stop, "hammertime"})
 
-# IO.inspect PledgeServer.recent_pledge()
+ IO.inspect(PledgeServer.create_pledge("alde1", 10))
+ IO.inspect(PledgeServer.create_pledge("alde2", 10))
+ IO.inspect(PledgeServer.create_pledge("alde3", 10))
+ IO.inspect(PledgeServer.create_pledge("alde4", 10))
+ IO.inspect(PledgeServer.create_pledge("alde5", 10))
+
+ IO.inspect(PledgeServer.recent_pledge())
+
+ IO.inspect(PledgeServer.total_pledged())
+ IO.inspect(Process.info(pid, :messages))
