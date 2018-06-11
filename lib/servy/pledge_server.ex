8476544926393a60e809680a2a9c5dc28d16f621
alde103 <aldebaran_103@hotmail.com>
2018-06-10 21:03:46 -0500

    24-3

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,36 +1,59 @@
 defmodule Servy.PledgeServer do
+  @name :pledge_server
+  def start do
+    IO.puts("Starting the server")
+    pid = spawn(__MODULE__, :listen_loop, [[]])
+    Process.register(pid, @name)
+    pid
+  end
   def listen_loop(state) do
     IO.puts("\n Waiting for a message...")
-
     receive do
-      {:create_pledge, name, amount} ->
+      {sender, :create_pledge, name, amount} ->
         {:ok, id} = send_pledge_to_service(name, amount)
-        new_state = [{name, amount} | state]
-        IO.puts("#{name} pledge #{amount}!")
-        IO.puts("New state is #{inspect new_state}")
+        most_rec_pledges = Enum.take(state, 2)
+        new_state = [{name, amount} | most_rec_pledges]
+        send(sender, {:response, id})
         listen_loop(new_state)
-      {:recent_pledges, pid} ->
-        cache = sim_cache(state, 0)
-        send(pid, {:response, cache})
+      {sender, :recent_pledges} ->
+        send(sender, {:response, state})
         listen_loop(state)
     end
   end
 
-  # def create_pledge(name,amount) do
-  #   {:ok, id} = send_pledge_to_service(name,amount)
-  # end
+   def create_pledge(name,amount) do
+     send(@name, {self(), :create_pledge, name, amount})
+     receive do {:response, status} -> status end
+   end
+
+   def recent_pledge() do
+    send(@name, {self(), :recent_pledges})
+    receive do {:response, pledges} -> pledges end
+   end
 
    def send_pledge_to_service(_name, _amount) do
      {:ok, "pledge-#{:rand.uniform(1000)}"}
    end
 
    #devuelve los primeros 3 elementos
-   def sim_cache([h |_t], 3) do [h] end
+   def sim_cache([h |_t], 2) do [h] end
 
    def sim_cache([], _n) do [] end
 
-   def sim_cache([h | t], n) when n < 3 do
+   def sim_cache([h | t], n) when n < 2 do
       [h] ++ sim_cache(t, n + 1)
    end
 
 end
+
+# alias Servy.PledgeServer
+
+# pid = PledgeServer.start()
+
+# IO.inspect(PledgeServer.create_pledge("alde1", 10))
+# IO.inspect(PledgeServer.create_pledge("alde2", 10))
+# IO.inspect(PledgeServer.create_pledge("alde3", 10))
+# IO.inspect(PledgeServer.create_pledge("alde4", 10))
+# IO.inspect(PledgeServer.create_pledge("alde5", 10))
+
+# IO.inspect PledgeServer.recent_pledge()
